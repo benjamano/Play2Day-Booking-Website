8768b6fb79feca6b5c654980fa68405c5b65cd1c
Ben Mercer <104690692+benjamano@users.noreply.github.com>
2023-10-21 14:11:12 +0100

    Adding a super secret dev menu

--- mysite/__init__.py
+++ mysite/__init__.py
@@ -1569,6 +1569,14 @@ def mark_arrived():
     else:
         return redirect(url_for("managereditbooking"))
 
+# ---------------------------------------------------------------------------------------------------------------- #
+
+@app.route("/account/developer")
+def devtest():
+    
+    return render_template("/manager/devtest.html")
+
+
 if __name__ == '__main__':
     #db.create_all()
     app.run(host="0.0.0.0", port=5000, debug=True)
